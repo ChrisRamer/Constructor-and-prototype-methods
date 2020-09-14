46a568b9c6b374d0e5cb7c9f19e0ef215444176d
Chris Ramer <chrisramer.97@gmail.com>
2020-09-14 14:09:43 -0700

    Basic constructor for a contact's first & last name and phone number

--- js/scripts.js
+++ js/scripts.js
@@ -1,5 +1,16 @@
 $(document).ready(function () {
 
-	//
+	function Contact(firstName, lastName, phoneNumber) {
+		this.firstName = firstName;
+		this.lastName = lastName;
+		this.phoneNumber = phoneNumber;
+	}
+
+	Contact.prototype.fullName = function() {
+		return this.firstName + " " + this.lastName;
+	}
+
+	let testContact = new Contact("Loki", "Odinson", "420-420-6969");
+	console.log("Full name is: " + testContact.fullName());
 
 });
