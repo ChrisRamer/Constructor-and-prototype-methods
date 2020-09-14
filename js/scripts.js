8d0c1e674edde3c38de3987b3f69112789cafbb3
Chris Ramer <chrisramer.97@gmail.com>
2020-09-14 14:26:28 -0700

    Add address book constructor

--- js/scripts.js
+++ js/scripts.js
@@ -1,5 +1,13 @@
 $(document).ready(function () {
 
+	function AddressBook() {
+		this.contacts = [];
+	}
+
+	AddressBook.prototype.addContact = function(contact) {
+		this.contacts.push(contact);
+	}
+
 	function Contact(firstName, lastName, phoneNumber) {
 		this.firstName = firstName;
 		this.lastName = lastName;
@@ -10,7 +18,10 @@ $(document).ready(function () {
 		return this.firstName + " " + this.lastName;
 	}
 
-	let testContact = new Contact("Loki", "Odinson", "420-420-6969");
-	console.log("Full name is: " + testContact.fullName());
+	let addressBook = new AddressBook();
+	let contact1 = new Contact("Loki", "Odinson", "420-420-6969");
+	let contact2 = new Contact("Thor", "Odinson", "420-420-6969");
+	addressBook.addContact(contact1);
+	addressBook.addContact(contact2);
 
 });
